29cbe1104301353eb8a92722fda52bc41da1e4d0
kgarousi <kgarousi32@gmail.com>
2022-04-28 18:22:32 -0500

    add logic for all game outcomes

--- script.js
+++ script.js
@@ -22,10 +22,25 @@ function playRound(){
     let cpuChoiceUpper = cpuChoice.toUpperCase();
 
     if(cpuChoiceUpper == playerChoiceUpper){
-        return "It's a tie"
+        return "You both chose " + playerChoice + " It's a tie"
     }
-    else{
-        return "It's not a tie"
+    else if(cpuChoiceUpper == "ROCK"  && playerChoiceUpper == "PAPER"){
+        return "Paper covers rock, you win!";
+    }
+    else if(cpuChoiceUpper == "PAPER"  && playerChoiceUpper == "ROCK"){
+        return "Paper covers rock, you lose";
+    }
+    else if(cpuChoiceUpper == "SCISSORS"  && playerChoiceUpper == "ROCK"){
+        return "Rock beats scissors, you win!";
+    }
+    else if(cpuChoiceUpper == "ROCK"  && playerChoiceUpper == "SCISSORS"){
+        return "Rock beats scissors, you lose";
+    }
+    else if(cpuChoiceUpper == "SCISSORS"  && playerChoiceUpper == "PAPER"){
+        return "Scissors cuts paper, you lose";
+    }
+    else if(cpuChoiceUpper == "PAPER"  && playerChoiceUpper == "SCISSORS"){
+        return "Paper covers rock, you win!";
     }
 }
 function getRandomNumber(max){
