c18505a40375f82765c52dca446551b2cc7fcc85
kgarousi <kgarousi32@gmail.com>
2022-04-28 16:55:05 -0500

    add semicolons, add more descriptive output

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-game()
+game();
 function computerPlay(){
     let opponent_choice = getRandomNumber(3)
     if(opponent_choice == 0){
-        return "Rock"
+        return "Rock";
     }
     else if(opponent_choice == 1){
-        return "Paper"
+        return "Paper";
     }
     else{
-        return "Scissors"
+        return "Scissors";
     }
 }
 function playRound(){
-    let player_choice = "Rock"
+    let player_choice = "Rock";
     let cpu_choice = computerPlay()
-    console.log(cpu_choice)
+    console.log("Opponent's choice: " + cpu_choice)
     if(cpu_choice == "Rock"){
-        return "You both chose rock, it's a tie."
+        return "You both chose rock, it's a tie.";
     }
     else if(cpu_choice == "Paper"){
-        return "Paper covers rock, you lose!"
+        return "Paper covers rock, you lose!";
     }
     else{
-        return "Rock beats scissors, you win!"
+        return "Rock beats scissors, you win!";
     }
 }
 function getRandomNumber(max){
-    return Math.floor(Math.random() * max)
+    return Math.floor(Math.random() * max);
 }
 function game(){
     for(let i = 0; i < 5; i++){
-        console.log(playRound())
+        console.log(playRound());
     }
 }
