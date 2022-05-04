f225db62f0d14ea32e0800fbbdbdb0e7051c5c05
kgarousi <kgarousi32@gmail.com>
2022-05-04 17:37:45 -0500

    add score tracking

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 game();
+let playerScore = 0;
+let cpuScore = 0;
 function computerPlay(){
     let opponentChoice = getRandomNumber(3)
     if(opponentChoice == 0){
@@ -17,45 +19,60 @@ function playRound(playerChoice){
     const playerDisplay = document.createElement('h3');
     const computerDisplay = document.createElement('h3');
     const results = document.createElement('h3');
-
+    const score = document.createElement('h3');
     if(cpuChoice == playerChoice){
         playerDisplay.textContent = "Player choice: " + playerChoice;
         computerDisplay.textContent = "Computer choice: " + cpuChoice;
         results.textContent = "You both chose " + playerChoice + ", it's a tie ";
+        score.textContent = "Player Score: " + playerScore + " Computer Score: " + cpuScore;
     }
    else if(cpuChoice == "Rock"  && playerChoice == "Paper"){
+        playerScore++;
         playerDisplay.textContent = "Player choice: " + playerChoice;
         computerDisplay.textContent = "Computer choice: " + cpuChoice;
         results.textContent = "Paper covers rock, you win!";
+        score.textContent = "Player Score: " + playerScore + " Computer Score: " + cpuScore;
     }
     else if(cpuChoice == "Paper"  && playerChoice == "Rock"){
+        cpuScore++;
         playerDisplay.textContent = "Player choice: " + playerChoice;
         computerDisplay.textContent = "Computer choice: " + cpuChoice;
-        results.textContent = "Paper covers rock, you lose!"
+        results.textContent = "Paper covers rock, you lose!";
+        score.textContent = "Player Score: " + playerScore + " Computer Score: " + cpuScore;
     }
     else if(cpuChoice == "Scissors"  && playerChoice == "Rock"){
+        playerScore++;
+        console.log(playerScore)
         playerDisplay.textContent = "Player choice: " + playerChoice;
         computerDisplay.textContent = "Computer choice: " + cpuChoice;
         results.textContent = "Rock beats scissors, you win!";
+        score.textContent = "Player Score: " + playerScore + " Computer Score: " + cpuScore;
     }   
     else if(cpuChoice == "Rock"  && playerChoice == "Scissors"){
+        cpuScore++;
         playerDisplay.textContent = "Player choice: " + playerChoice;
         computerDisplay.textContent = "Computer choice: " + cpuChoice;
         results.textContent = "Rock beats scissors, you lose";
+        score.textContent = "Player Score: " + playerScore + " Computer Score: " + cpuScore;
     }
     else if(cpuChoice == "Scissors"  && playerChoice == "Paper"){
+        cpuScore++;
         playerDisplay.textContent = "Player choice: " + playerChoice;
         computerDisplay.textContent = "Computer choice: " + cpuChoice;
         results.textContent = "Scissors beats paper, you lose";
+        score.textContent = "Player Score: " + playerScore + " Computer Score: " + cpuScore;
     }
     else if(cpuChoice == "Paper"  && playerChoice == "Scissors"){
+        playerScore++;
         playerDisplay.textContent = "Player choice: " + playerChoice;
         computerDisplay.textContent = "Computer choice: " + cpuChoice;
         results.textContent = "Scissors beats paper, you win! ";
+        score.textContent = "Player Score: " + playerScore + " Computer Score: " + cpuScore;
     }
     container.appendChild(playerDisplay);
     container.appendChild(computerDisplay);
     container.appendChild(results);
+    container.appendChild(score);
 }
 function getRandomNumber(max){
     return Math.floor(Math.random() * max);
