b73c80998fcafebbbda4ec959f68619e3a1eda1e
kgarousi <kgarousi32@gmail.com>
2022-05-04 13:46:59 -0500

    remove casesenitive protection, not needed for btn

--- script.js
+++ script.js
@@ -17,29 +17,26 @@ function playRound(playerChoice){
     console.log("Player's choice: " + playerChoice);
     console.log("Opponent's choice: " + cpuChoice);
     
-    let playerChoiceUpper = playerChoice.toUpperCase();
-    let cpuChoiceUpper = cpuChoice.toUpperCase();
-
-    if(cpuChoiceUpper == playerChoiceUpper){
-        return "You both chose " + cpuChoice + " It's a tie"
+    if(cpuChoice == playerChoice){
+        console.log("You both chose " + cpuChoice + ", it's a tie");
     }
-    else if(cpuChoiceUpper == "ROCK"  && playerChoiceUpper == "PAPER"){
-        return "Paper covers rock, you win!";
+    else if(cpuChoice == "Rock"  && playerChoice == "Paper"){
+        console.log("Paper covers rock, you win!");
     }
-    else if(cpuChoiceUpper == "PAPER"  && playerChoiceUpper == "ROCK"){
-        return "Paper covers rock, you lose";
+    else if(cpuChoice == "Paper"  && playerChoice == "Rock"){
+        console.log("Paper covers rock, you lose");
     }
-    else if(cpuChoiceUpper == "SCISSORS"  && playerChoiceUpper == "ROCK"){
-        return "Rock beats scissors, you win!";
+    else if(cpuChoice == "Scissors"  && playerChoice == "Rock"){
+        console.log("Rock beats scissors, you win!");
     }
-    else if(cpuChoiceUpper == "ROCK"  && playerChoiceUpper == "SCISSORS"){
-        return "Rock beats scissors, you lose";
+    else if(cpuChoice == "Rock"  && playerChoice == "Scissors"){
+        console.log("Rock beats scissors, you lose");
     }
-    else if(cpuChoiceUpper == "SCISSORS"  && playerChoiceUpper == "PAPER"){
-        return "Scissors cuts paper, you lose";
+    else if(cpuChoice == "Scissors"  && playerChoice == "Paper"){
+        console.log("Scissors cuts paper, you lose");
     }
-    else if(cpuChoiceUpper == "PAPER"  && playerChoiceUpper == "SCISSORS"){
-        return "Paper covers rock, you win!";
+    else if(cpuChoice == "Paper"  && playerChoice == "Scissors"){
+        console.log("Paper covers rock, you win!");
     }
 }
 function getRandomNumber(max){
