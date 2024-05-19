c41b2f63c0bef1e2d952ccc1d60b925a6a0cfd98
kgarousi <kgarousi32@gmail.com>
2024-05-19 16:45:30 -0500

    added end game results to ui

--- script.js
+++ script.js
@@ -31,58 +31,66 @@ function playGame(){
    
     function playRound(humanSelection, computerSelection){
 
+        const score = document.querySelector("#score");
+
         if(humanSelection == "rock" && computerSelection == "paper"){
             computerScore++;
+            score.textContent = getScore(humanScore, computerScore);
             return "Paper beats rock, you lose!";
         }
         else if(humanSelection == "paper" && computerSelection == "scissors"){
             computerScore++;
+            score.textContent = getScore(humanScore, computerScore);
             return "Scissors beat paper, you lose!";
         }
         else if(humanSelection == "scissors" && computerSelection == "rock"){
             
             computerScore++;
+            score.textContent = getScore(humanScore, computerScore);
             return "Rock beats scissors, you lose!";
         }
         else if(humanSelection == "scissors" && computerSelection == "paper"){
             humanScore++;
+            score.textContent = getScore(humanScore, computerScore);
             return "Scissors beats paper, you win!";
         }
         else if(humanSelection == "paper" && computerSelection == "rock"){
             humanScore++;
+            score.textContent = getScore(humanScore, computerScore);
             return "Paper beats rock, you win!";
         }
         else if(humanSelection == "rock" && computerSelection == "scissors"){
             humanScore++;
+            score.textContent = getScore(humanScore, computerScore);
             return "Rock beats scissors, you win!";
         }
         else{
             return "You both picked the same, it's a tie!";
         }
-       results.textContent = getScore(humanScore, computerScore)
     }
-     function getScore(humanScore, computerScore){    
-        if(humanScore < 5 || computerScore < 5){  
+     function getScore(humanScore, computerScore){
+        while(humanScore < 5 && computerScore < 5){  
             if(humanScore < computerScore){
-                    return "Player score: " + humanScore + " Computer score: " + computerScore + " You lost the round!";
-                }
-                else if(humanScore > computerScore){
-                    return "Player score: " + humanScore + " Computer score: " + computerScore +  " You won the game!";
-                }
-                
-                else if(humanScore == computerScore){
-                    return "Player score: " + humanScore + " Computer score: " + computerScore + " It's a tie!";
-                }
+                return "Player score: " + humanScore + " Computer score: " + computerScore;
+            }
+            else if(humanScore > computerScore){
+                return "Player score: " + humanScore + " Computer score: " + computerScore ;
+            }
+            
+            else if(humanScore == computerScore){
+                return "Player score: " + humanScore + " Computer score: " + computerScore ;
+            }
         } 
-        displayResults(humanScore, computerScore)   
+        const finalScore = document.querySelector("#final-score");
+        finalScore.textContent = displayResults(humanScore, computerScore);
     }
 
     function displayResults(humanScore, computerScore){
         if(humanScore == 5){
-            console.log("The game is over, you won! Final Score: You: " + humanScore + " Computer Score: " + computerScore);
+            return "The game is over, you won! Final Score: You: " + humanScore + " Computer Score: " + computerScore;
         }
         else if(computerScore == 5){
-            console.log("The game is over, you lost :(. Final Score: You: " + humanScore + " Computer Score: " + computerScore);
+            return "The game is over, you lost :(. Final Score: You: " + humanScore + " Computer Score: " + computerScore;
         }
     }
 }
